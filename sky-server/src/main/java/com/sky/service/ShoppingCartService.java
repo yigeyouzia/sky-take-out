ca00bb2811fcd749d952407536174c3076772081
cyt <1793402316@qq.com>
2024-10-29 19:01:50 +0800

    day7 add sub clean购物车✨

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 /**
  * @author cyt
@@ -14,4 +17,14 @@ public interface ShoppingCartService {
      * @return
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    List<ShoppingCart> list();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    void cleanShoppingCart();
 }
